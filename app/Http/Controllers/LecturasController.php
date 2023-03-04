007b23d625c54bd5c5607a23d53f181878edb473
WillPozo <pchp@will>
2023-03-04 10:03:28 -0500

    probando}

---
+++ app/Http/Controllers/LecturasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lecturas;
+use Illuminate\Http\Request;
+
+class LecturasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lecturas  $lecturas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lecturas $lecturas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lecturas  $lecturas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lecturas $lecturas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lecturas  $lecturas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lecturas $lecturas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lecturas  $lecturas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lecturas $lecturas)
+    {
+        //
+    }
+}
---
+++ app/Models/Lecturas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lecturas extends Model
+{
+    use HasFactory;
+}
