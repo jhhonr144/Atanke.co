401036de05cb17f5374a37dcab6784b9cbb46799
elpozxo <wjpozo98@gmail.com>
2023-03-28 15:34:33 -0500

    Sorry user login/create

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,84 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Datos;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Laravel\Sanctum\Sanctum;
-
-
-
-use Illuminate\Support\Facades\Storage;
 
 class LoginController extends Controller
-{
-    public function registro(Request $request)
-    {
-
-        $datos = new Datos();
-        try {
-            $validar = Validator::make(
-                $request->all(),
-                [
-                    'name' => 'required|string|max:225',
-                    'email' => 'required|string|email|max:225|unique:users',
-                    'password' => 'required|string|min:6',
-                ]
-            );
-            if ($validar->fails()) {
-                return response()->json($validar->errors());
-            }
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' =>  Hash::make($request->password),
-                'r_users_roles' => 1,
-                'r_users_estados' => 1
-            ]);
-            $token = $user->CreateToken('auth_token')->plainTextToken;
-            $datos->id = 0;
-            $datos->mensaje = $token;
-            $datos->datos = $user;
-            $datos->datos_len = $user->id;
-        } catch (\Exception $e) {
-            $datos->id = 1;
-            $datos->mensaje = "Error crear user\n" . $e;
-        }
-        return response()->json($datos);
-    }
-
-
-    public function login(Request $request)
-    {
-        $datos = new Datos();
-        try {
-            if (!Auth::attempt($request->only('email', 'password'))) {
-                $datos->id = 1;
-                $datos->mensaje = "Error al logearse.";
-                return response()->json($datos);
-            }
-            $user = User::where('email', $request->email)->firstOrFail();
-            $token = $user->CreateToken('auth_token')->plainTextToken;
-            if($user->elEstado->nombre=='activo'){
-                $user->remember_token=$token;
-                $user->save();
-                $datos->id = 0;
-                $datos->mensaje = $token;
-                $datos->datos = $user;
-                $datos->datos_len = $user->id;
-            }
-            else{
-                $datos->id = 1;
-                $datos->mensaje = "usuario Inactivo"; 
-            }
-        } catch (\Exception $e) {
-            $datos->id = 1;
-            $datos->mensaje = "Error crear user\n" . $e;
-        }
-        return response()->json($datos);
-    }
+{   
     public function salir(Request $request)
     {
         $user = $request->user('sanctum');
---
+++ app/Http/Controllers/User/Registro/CrearCuentaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\User\Registro;
+
+use App\Http\Controllers\Controller;
+use App\Models\Datos;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CrearCuentaController extends Controller
+{
+    public function registro(Request $request)
+    {
+        $datos = new Datos();
+        try {
+            $validar = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required|string|max:225',
+                    'email' => 'required|string|email|max:225|unique:users',
+                    'password' => 'required|string|min:6',
+                ]
+            );
+            if ($validar->fails()) {
+                $datos->id = 1;
+                $datos->mensaje = "Error por datos erroneos";
+                $datos->datos = $validar->errors();
+            } else {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' =>  Hash::make($request->password),
+                    'r_users_roles' => 1,
+                    'r_users_estados' => 1
+                ]);
+                $token = $user->CreateToken('auth_token')->plainTextToken;
+                $datos->id = 0;
+                $datos->mensaje = $token;
+                $datos->datos = $user;
+                $datos->datos_len = $user->id;
+            }
+        } catch (\Exception $e) {
+            $datos->id = 1;
+            $datos->mensaje = "Error crear user\n" . $e;
+        }
+        return response()->json($datos);
+    }
+}
