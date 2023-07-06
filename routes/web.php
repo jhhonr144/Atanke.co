f9a9ae13548cb2a1cc5d821efed7ed4dfad0b946
WillPozo <wjpozo98@gmail.com>
2023-07-06 18:40:28 -0500

    ultimo retoke

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return ('Desarrollado por WillPozo,@ derecho reservado');
-    
+    return redirect("https://atanke.org/APP.APK");
+});  
+Route::get('/Dev', function () {
+    return ('Desarrollado por WillPozo,@derecho reservado'); 
 });  
